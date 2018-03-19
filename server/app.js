a910975899703b9c3f0fcc16678e9d92d2a10d84
Chris Beard <coebe500@aol.com>
2018-03-19 18:27:28 +0000

    Init schema

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
+app.use("/graphql", graphqlHTTP({
+    
+}));
+
 const port = 4000;
 app.listen(port, () => {
     console.log("express server started on port" + port );
