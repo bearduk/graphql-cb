e08a66a096d5f09b9959ca08b461dbb2da3d2e24
Chris Beard <coebe500@aol.com>
2018-04-10 15:04:45 +0100

    apollo and cors setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
 // Components
 import BookList from './components/BookList';
+
+// apollo cleint setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 class App extends Component {
   render() {
     return (
+
+  <ApolloProvider client={client}>
+
       <div id="main">
         <h1>graphql-cb reading list project</h1>
         <p>Using graph-ql, mLab and react.</p>
         <BookList />
       </div>
-    );
+    
+  </ApolloProvider>
+  
+  );
   }
 }
 
